f0e2a621c1a71763b537b7b35d6395963b32da4f
Jinho Lee <jl5027@columbia.edu>
2022-04-24 18:52:59 +0900

    Update 활주로건설.py

--- 활주로건설.py
+++ 활주로건설.py
@@ -1,104 +1,68 @@
 import sys
 
 class Solution:
+    def __init__(self):
+        self.runway = 0
+
     def do(self, T, N, X, mat):
-        ROWS, COLS = N, N
-        L = X
-        runway = 0
+        rowMat = [mat[r][:] for r in range(N)]
+        colMat = [[mat[r][c] for r in range(N)] for c in range(N)]
 
-        for r in range(ROWS):
-            prvHeight, length, complete, putRamp = mat[r][0], 1, True, False
-            for c in range(1, COLS):
-                curHeight = mat[r][c]
-                diff = abs(prvHeight - curHeight)
-                if diff == 0:
-                    length += 1
+        for n in range(N):
+            self.check(rowMat[n], X)
 
-                elif diff > 1:
-                    complete = False
-                    break
+        for n in range(N):
+            self.check(colMat[n], X)
 
-                else:
-                    if prvHeight > curHeight:
-                        if putRamp:
-                            if length < L:
-                                complete = False
-                                break
-                            else:
-                                putRamp = False
+        return f"#{T} {self.runway}"
 
-                        length = 1
-                        putRamp = True
+    def check(self, arr, L):
+        X = L
+        idx = len(arr)
+        prvHeight, length, complete, putRamp = arr[0], 1, True, False
+        for i in range(1, idx):
+            curHeight = arr[i]
+            diff = abs(prvHeight - curHeight)
+            if diff == 0:
+                length += 1
 
-                    elif prvHeight < curHeight:
-                        if length < L:
-                            complete = False
-                            break
+            elif diff > 1:
+                complete = False
+                break
 
-                        length = 1
-
-                if length == L and putRamp:
-                    length = 0
-                    putRamp = False
-
-                prvHeight = curHeight
-
-                if c == COLS - 1:
+            else:
+                if prvHeight > curHeight:
                     if putRamp:
                         if length < L:
                             complete = False
                             break
+                        else:
+                            putRamp = False
 
-            if complete:
-                runway += 1
+                    length = 1
+                    putRamp = True
 
-        for c in range(COLS):
-            prvHeight, length, complete, putRamp = mat[0][c], 1, True, False
-            for r in range(1, ROWS):
-                curHeight = mat[r][c]
-                diff = abs(prvHeight - curHeight)
-                if diff == 0:
-                    length += 1
+                elif prvHeight < curHeight:
+                    if length < L:
+                        complete = False
+                        break
 
-                elif diff > 1:
-                    complete = False
-                    break
+                    length = 1
 
-                else:
-                    if prvHeight > curHeight:
-                        if putRamp:
-                            if length < L:
-                                complete = False
-                                break
-                            else:
-                                putRamp = False
+            if length == L and putRamp:
+                length = 0
+                putRamp = False
 
-                        length = 1
-                        putRamp = True
+            prvHeight = curHeight
 
-                    elif prvHeight < curHeight:
-                        if length < L:
-                            complete = False
-                            break
+            if i == idx - 1:
+                if putRamp:
+                    if length < L:
+                        complete = False
+                        break
 
-                        length = 1
-
-                if length == L and putRamp:
-                    length = 0
-                    putRamp = False
-
-                prvHeight = curHeight
-
-                if r == ROWS - 1:
-                    if putRamp:
-                        if length < L:
-                            complete = False
-                            break
-
-            if complete:
-                runway += 1
-
-        return f"#{T} {runway}"
+        if complete:
+            self.runway += 1
 
 sys.stdin = open("runway_input.txt", "r")
 T = int(input())
@@ -109,4 +73,5 @@ for test_case in range(1, T + 1):
     N, X = map(int, input().split())
     mat = [list(map(int, input().split())) for _ in range(N)]
     print(sol.do(test_case, N, X, mat))
+    sol.runway = 0
     # ///////////////////////////////////////////////////////////////////////////////////
